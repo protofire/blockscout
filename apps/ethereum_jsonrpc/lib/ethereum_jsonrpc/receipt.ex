3e78a8ae05ca0d75c75429ee637237cb77ecdb11
Den Smalonski <dzianis.smalonski@protofire.io>
2024-04-09 15:11:32 +0200

    fix(receipt): temp fix with one1

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
@@ -259,7 +259,11 @@ defmodule EthereumJSONRPC.Receipt do
 
   defp entry_to_elixir({key, value})
        when key in ~w(from to) do
-    result = decode_bech_32(value)
+    result = if String.starts_with?(value, "one1") do
+      decode_bech_32(value)
+    else
+      value
+    end
     {:ok, {key, result}}
   end
 

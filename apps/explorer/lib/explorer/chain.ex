9b305596d84587848b17491dcf544a74d84c2c2f
Den Smalonski <dzianis.smalonski@protofire.io>
2024-07-08 09:14:15 +0200

    fix: commit filtering only block with tx

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -1478,14 +1478,14 @@ defmodule Explorer.Chain do
     end
   end
 
-  defp with_transactions(query, true) do
-    from(q in query,
-      as: :block,
-      where: exists(from(t in Transaction, where: t.block_hash == parent_as(:block).hash))
-    )
-  end
+  # defp with_transactions(query, true) do
+  #   from(q in query,
+  #     as: :block,
+  #     where: exists(from(t in Transaction, where: t.block_hash == parent_as(:block).hash))
+  #   )
+  # end
 
-  defp with_transactions(query, false), do: query
+  # defp with_transactions(query, false), do: query
 
   @doc """
   Map `block_number`s to their `t:Explorer.Chain.Block.t/0` `hash` `t:Explorer.Chain.Hash.Full.t/0`.

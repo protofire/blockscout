a6c98892caf4d6c320e9e197b19526efea8827c4
goxibyte <denissmolonski@gmail.com>
2025-02-24 17:37:35 +0100

    fix: return back

--- apps/ethereum_jsonrpc/config/prod.exs
+++ apps/ethereum_jsonrpc/config/prod.exs
@@ -1,8 +1,8 @@
 import Config
 
-config :ethereum_jsonrpc, EthereumJSONRPC.Tracer, env: "production", disabled?: false
+config :ethereum_jsonrpc, EthereumJSONRPC.Tracer, env: "production", disabled?: true
 
 config :logger, :ethereum_jsonrpc,
-  level: :debug,
+  level: :info,
   path: Path.absname("logs/prod/ethereum_jsonrpc.log"),
   rotate: %{max_bytes: 52_428_800, keep: 19}
